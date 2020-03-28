0e68dc64232a7428ca88440bbd34947eaf3e0884
Latesh-Kumari <latesh.kumari_cs19@gla.ac.in>
2020-03-28 15:11:26 +0530

    implement TreeNode class

--- src/problem1/node/TreeNode.java
+++ src/problem1/node/TreeNode.java
@@ -7,4 +7,11 @@
 package problem1.node;
 
 public class TreeNode {
+    public int value;
+    public TreeNode left;
+    public TreeNode right;
+
+    public TreeNode(int value) {
+        this.value = value;
+    }
 }
